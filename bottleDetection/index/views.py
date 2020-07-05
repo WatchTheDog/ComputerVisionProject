5eb268551f8cbcd61b022b6e01c4bf0bb6d5bcd2
Timo Achtelik <timo.achtelik@stud.h-da.de>
2020-07-05 18:29:28 +0200

    new version

--- bottleDetection/index/views.py
+++ bottleDetection/index/views.py
@@ -1,18 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("<h1>Computer Vision: Freie Aufgabe</h1><br/><h2>Bottle Detection mit Instagram Bildern</h2>")
-
-
-def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
-
-
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
-
-
-def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    return render(request, 'index/index.html', {})
