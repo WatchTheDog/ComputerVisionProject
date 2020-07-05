1966deaa9c9278cbd804c1018b859516f79e386b
Timo Achtelik <timo.achtelik@stud.h-da.de>
2020-07-05 18:28:57 +0200

    new style and content

--- bottleDetection/bottleDetection/urls.py
+++ bottleDetection/bottleDetection/urls.py
@@ -3,5 +3,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('index.urls')),
+    path('impressum/', include('impressum.urls')),
+    path('datenschutz/', include('datenschutz.urls')),
     path('admin/', admin.site.urls),
 ]
