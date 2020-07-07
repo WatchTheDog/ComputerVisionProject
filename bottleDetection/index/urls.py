25263f36cdcd8f52ddb759f7511f0de4339d0b81
Timo Achtelik <timo.achtelik@stud.h-da.de>
2020-07-07 20:57:17 +0200

    deleted settings

--- bottleDetection/index/urls.py
+++ bottleDetection/index/urls.py
@@ -5,6 +5,3 @@ urlpatterns = [
     path('', views.index, name='index'),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,
-                          document_root=settings.MEDIA_ROOT)
