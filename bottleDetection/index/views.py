925d552e9f252ca6550544e6b8144feaebf1ebdd
Timo Achtelik <timo.achtelik@stud.h-da.de>
2020-07-07 20:45:11 +0200

    testing upload on server

--- bottleDetection/index/views.py
+++ bottleDetection/index/views.py
@@ -1,5 +1,19 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .forms import *
+
+
+# Create your views here.
+def search_image_view(request):
+    if request.method == 'POST':
+        form = SearchImageForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            form.save()
+            return redirect('success')
+    else:
+        form = SearchImageForm()
+    return render(request, 'index.html', {'form': form})
 
 
 def index(request):
